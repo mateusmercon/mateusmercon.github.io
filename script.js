b12d08c77518640d55316af92f41a7701eb2208f
Mateus Merçon <mateusmercon@gmail.com>
2024-01-14 21:08:20 -0300

    Inserindo projetos e melhorias visuais

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ document.addEventListener('click', (event) => {
         icon.classList.remove("open")
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var navbar = document.getElementById("desktop-nav");
+    var hamburger = document.getElementById("hamburger-nav");
+    var initialHeight = 10;
+
+    window.addEventListener("scroll", function () {
+        // Ajusta o height quando a rolagem atinge 5
+        if (window.scrollY > 5) {
+            navbar.style.position = "fixed";
+            navbar.style.top = "0";
+            navbar.style.height = "5vh";
+
+        } else {
+            navbar.style.position = "static";
+            navbar.style.top = "auto";
+            navbar.style.height = initialHeight + "vh";
+
+        }
+    });
+});
